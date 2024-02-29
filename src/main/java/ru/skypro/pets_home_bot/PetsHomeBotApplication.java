4b72b9ad5f785f3a7f8722e8f38b5861c4b01532
user <rusobraz@mail.ru>
2024-02-29 23:50:22 +0300

    init

---
+++ src/main/java/ru/skypro/pets_home_bot/PetsHomeBotApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.pets_home_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsHomeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetsHomeBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/pets_home_bot/PetsHomeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.pets_home_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsHomeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
