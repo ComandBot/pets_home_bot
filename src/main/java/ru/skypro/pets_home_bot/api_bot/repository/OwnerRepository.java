93b56a783cb8bdcf5a7b9a08447053c70f7842e8
user <rusobraz@mail.ru>
2024-03-08 12:34:45 +0300

    4.1 Исправил OwnerRepository

--- src/main/java/ru/skypro/pets_home_bot/api_bot/repository/OwnerRepository.java
+++ src/main/java/ru/skypro/pets_home_bot/api_bot/repository/OwnerRepository.java
@@ -2,6 +2,7 @@ package ru.skypro.pets_home_bot.api_bot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.skypro.pets_home_bot.api_bot.model.Owner;
+import ru.skypro.pets_home_bot.api_bot.model.PetUserId;
 
-public interface OwnerRepository extends JpaRepository<Owner,Integer> {
+public interface OwnerRepository extends JpaRepository<Owner, PetUserId> {
 }
