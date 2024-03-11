8631f8eef8a20b0a5e782c639b38bd2951fbe276
user <rusobraz@mail.ru>
2024-03-11 21:37:49 +0300

    Добавил описание интерфейса сервиса PetUserService

--- src/main/java/ru/skypro/pets_home_bot/api_bot/service/PetUserService.java
+++ src/main/java/ru/skypro/pets_home_bot/api_bot/service/PetUserService.java
@@ -1,4 +1,27 @@
 package ru.skypro.pets_home_bot.api_bot.service;
 
+import ru.skypro.pets_home_bot.api_bot.model.PetUser;
+
 public interface PetUserService {
+    /**
+     * Регистрация нового усыновителя потенциального усыновителя
+     * @param petUser - ссылка на нового усыновителя
+     * @return - возвращает ссылку на нового усыновителя
+     */
+    PetUser add(PetUser petUser);
+
+    /**
+     * Убирает регистрацию усыновителя
+     * @param idPetUser - id усыновителя
+     * @return - возвращает удаленног усыновителя или null
+     */
+    PetUser deletePetUser(int idPetUser);
+
+    /**
+     * Поиск усыновителя(или владельца животного, если усыновитель является владельцем на испытательном сроке)
+     * по chatId, для связи с волонтером.
+     * @param chatIdPetUser - chatId усыновителя
+     * @return - возвращается
+     */
+    PetUser findByChatIdPetUser(int chatIdPetUser);
 }
--- src/main/java/ru/skypro/pets_home_bot/api_bot/service/impl/PetUserServiceImpl.java
+++ src/main/java/ru/skypro/pets_home_bot/api_bot/service/impl/PetUserServiceImpl.java
@@ -1,8 +1,23 @@
 package ru.skypro.pets_home_bot.api_bot.service.impl;
 
 import org.springframework.stereotype.Service;
+import ru.skypro.pets_home_bot.api_bot.model.PetUser;
 import ru.skypro.pets_home_bot.api_bot.service.PetUserService;
 
 @Service
 public class PetUserServiceImpl implements PetUserService {
+    @Override
+    public PetUser add(PetUser petUser) {
+        return null;
+    }
+
+    @Override
+    public PetUser deletePetUser(int idPetUser) {
+        return null;
+    }
+
+    @Override
+    public PetUser findByChatIdPetUser(int chatIdPetUser) {
+        return null;
+    }
 }
