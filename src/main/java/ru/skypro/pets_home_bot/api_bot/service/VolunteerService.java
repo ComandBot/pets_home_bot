f967e4441c6822fe8a00caab7f155e235f77f323
user <rusobraz@mail.ru>
2024-03-11 22:12:02 +0300

    Добавил описание интерфейса сервиса VolunteerService

--- src/main/java/ru/skypro/pets_home_bot/api_bot/service/VolunteerService.java
+++ src/main/java/ru/skypro/pets_home_bot/api_bot/service/VolunteerService.java
@@ -1,4 +1,38 @@
 package ru.skypro.pets_home_bot.api_bot.service;
 
+import ru.skypro.pets_home_bot.api_bot.model.PetUser;
+import ru.skypro.pets_home_bot.api_bot.model.Volunteer;
+
 public interface VolunteerService {
+
+    /**
+     * Регистрация волонтера
+     * @param volunteer - ссылка на нового волнтера
+     * @return - возвращает ссылку на новго волонтера
+     */
+    Volunteer add(Volunteer volunteer);
+
+    /**
+     * Удаление волонтера из системы приютов
+     * @param volunteerId - id удаляемого волонтера
+     * @return - возвращает удаленного волонтера млм null
+     */
+    Volunteer delete(int volunteerId);
+
+    /**
+     * При вызове волонтера усыновителем устанавливает связь с ним
+     * @param petUser - усыновитель вызывающий волонтера
+     * @param volunteerId - id волонтера с которым устанавливаем связь
+     * @return - возвращает отредактированного волонтера с полем workPetUserId не равным null,
+     * либо возвращает null если волонтер не найден
+     */
+    Volunteer setWorkPetUser(int volunteerId, PetUser petUser);
+
+    /**
+     * Если волонтер отработал с усыновителем, то высвобождаем волонтера
+     * @param volunteerId - id волонтера которого освобождаем
+     * @return - возвращает свободного волонтера у которого поле workPetUserId равно null
+     * либо возвращает null если волонтер не найден
+     */
+    Volunteer removeWorkPetUser(int volunteerId);
 }
--- src/main/java/ru/skypro/pets_home_bot/api_bot/service/impl/VolunteerServiceImpl.java
+++ src/main/java/ru/skypro/pets_home_bot/api_bot/service/impl/VolunteerServiceImpl.java
@@ -1,8 +1,29 @@
 package ru.skypro.pets_home_bot.api_bot.service.impl;
 
 import org.springframework.stereotype.Service;
+import ru.skypro.pets_home_bot.api_bot.model.PetUser;
+import ru.skypro.pets_home_bot.api_bot.model.Volunteer;
 import ru.skypro.pets_home_bot.api_bot.service.VolunteerService;
 
 @Service
 public class VolunteerServiceImpl implements VolunteerService {
+    @Override
+    public Volunteer add(Volunteer volunteer) {
+        return null;
+    }
+
+    @Override
+    public Volunteer delete(int volunteerId) {
+        return null;
+    }
+
+    @Override
+    public Volunteer setWorkPetUser(int volunteerId, PetUser petUser) {
+        return null;
+    }
+
+    @Override
+    public Volunteer removeWorkPetUser(int volunteerId) {
+        return null;
+    }
 }
