70e15b3db757be27fbefb91c512c3a3a226dcd0b
Sergey Volkov <rusobraz@mail.ru>
2024-03-31 15:27:28 +0300

    Добавил расшифровку консультатций

--- src/main/java/ru/skypro/pets_home_bot/api_bot/service/ConsultationService.java
+++ src/main/java/ru/skypro/pets_home_bot/api_bot/service/ConsultationService.java
@@ -3,7 +3,9 @@ package ru.skypro.pets_home_bot.api_bot.service;
 import ru.skypro.pets_home_bot.api_bot.model.Consultation;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ConsultationService {
     List<Consultation> findConsultationsByShelterId(int shelterId);
+    Optional<Consultation> findConsultationById(int consultationId);
 }
--- src/main/java/ru/skypro/pets_home_bot/api_bot/service/impl/ConsultationServiceImpl.java
+++ src/main/java/ru/skypro/pets_home_bot/api_bot/service/impl/ConsultationServiceImpl.java
@@ -6,6 +6,7 @@ import ru.skypro.pets_home_bot.api_bot.repository.ConsultationRepository;
 import ru.skypro.pets_home_bot.api_bot.service.ConsultationService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ConsultationServiceImpl implements ConsultationService {
@@ -19,5 +20,9 @@ public class ConsultationServiceImpl implements ConsultationService {
     @Override
     public List<Consultation> findConsultationsByShelterId(int shelterId) {
         return consultationRepository.findAllByShelterId(shelterId);
+    }
+    @Override
+    public Optional<Consultation> findConsultationById(int consultationId) {
+        return consultationRepository.findById(consultationId);
     }
 }
